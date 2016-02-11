b09bb195be779d687a8f50293297adda470e79bc
Toshi Wakita <wakita@cco.kanagawa-it.ac.jp>
2016-02-12 01:29:24 +0900

    end of sec9

---
+++ scripts/wall_trace.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+import rospy,copy, math
+from geometry_msgs.msg import Twist
+from std_srvs.srv import Trigger, TriggerResponse
+from pimouse_ros.msg import LightSensorValues
+
+class WallTrace():
+    def __init__(self):
+        self.cmd_vel = rospy.Publisher('/cmd_vel', Twist,queue_size=1)
+
+        self.sensor_values=LightSensorValues()
+        rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
+
+    def callback(self,messages):
+        self.sensor_values = messages
+
+    def run(self):
+        rate = rospy.Rate(10)
+        data = Twist()
+
+        accel = 0.02
+        data.linear.x = 0.0
+        while not rospy.is_shutdown():
+            s = self.sensor_values
+            data.linear.x += accel
+
+            if s.sum_forward >=30: data.linear.x = 0.0
+            elif data.linear.x <= 0.2: data.linear.x = 0.2
+            elif data.linear.x >= 0.4: data.linear.x = 0.4
+
+            if data.linear.x < 0.2: data.angular.z = 0.0
+            elif s.left_side < 1:  data.angular.z = 0.0
+            else:
+                target = 50
+                error = (target - s.left_side)/50.0
+                data.angular.z = error * 6 * math.pi / 180.0
+
+
+            self.cmd_vel.publish(data)
+            rate.sleep()
+
+
+            
+if __name__ == '__main__':
+    rospy.init_node('wall_trace')
+    rospy.wait_for_service('/motor_on')
+    rospy.wait_for_service('/motor_off')
+    rospy.on_shutdown(rospy.ServiceProxy('/motor_off', Trigger).call)
+    rospy.ServiceProxy('/motor_on',Trigger).call()
+    WallTrace().run()
+    
+
+    
